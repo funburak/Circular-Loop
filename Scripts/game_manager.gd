34050be85c1df1cc2b35042094fe0e22a17697cd
funburak <buraqdmrky@gmail.com>
2025-08-01 17:15:50 +0300

    Background added

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var radius = 120
+var radius = 530/2
 
-var center_left_circle = Vector2(200,200)
+var center_left_circle = Vector2(-382,220)
 var spawn_points_left_circle = [Vector2(50,40), Vector2(100,400), Vector2(300,400), Vector2(50,50)]
