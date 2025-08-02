b780093ecb59f884fb70c55ff3112a3247ff7758
Burak Demirkaya <buraqdmrky@gmail.com>
2025-08-02 21:45:03 +0300

    Wave-2 added

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -6,7 +6,7 @@ var center_left_circle = Vector2(-382,220)
 var center_right_circle = Vector2(388, 224)
 var spawn_points_left_circle = [Vector2(50,40), Vector2(100,400), Vector2(300,400), Vector2(50,50)]
 var rng = RandomNumberGenerator.new()
-var keys = ["Q", "W", "E", "R", "F"]
+var keys = ["Q", "W", "E"]
 var game_is_paused: bool = false
 var volume_float_value: float
 var total_point: int = 0
@@ -17,8 +17,8 @@ var enemy2_point: int = 0
 var enemy3_point: int = 0
 var enemy4_point: int = 0
 
-
 var current_wave = 1
+var max_wave = 3
 
 func get_random_key():
 	rng.randomize()
